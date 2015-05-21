00782ffbf015844d1a070c37acec71840bc7fefd
Jake Obron <jbobron@gmail.com>
2015-05-21 09:49:52 -0700

    creating repo

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var rimraf = require('gulp-rimraf');
+var runSequence = require('run-sequence');
+
+gulp.task('browserify', function(){
+  gulp.src('src/App.js')
+    .pipe(browserify({transform: 'reactify'}))
+    .pipe(concat('App.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('./dist/**/*.*', {read: false}) // much faster
+    .pipe(rimraf());
+})
+
+gulp.task('copy', function(){
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/styles/*.css')
+    .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/**/*.*', ['default']);
+});
+
+gulp.task('default', function(cb) {
+  runSequence('clean', 'browserify', 'copy', 'watch', cb);
+});
---
+++ src/App.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var Parent = require('./components/Parent');
+
+React.render(<Parent />, document.getElementById('app'));
