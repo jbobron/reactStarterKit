c6bebc66b957f297702e930018de311f18befe6d
Jake Obron <jbobron@gmail.com>
2015-05-21 10:11:01 -0700

    working

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,1 +1,19 @@
 var React = require('react');
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+module.exports = ShowList;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,1 +1,22 @@
 var React = require('react');
+var ShowList = require('./Child');
+
+var FriendsContainer = React.createClass({
+  getInitialState: function(){
+    return {
+      name: 'Steph Curry',
+      friends: ['Klay Thompson', 'Draymond Green', 'Andre Iguodala']
+    }
+  },
+  render: function(){
+    return (
+      <div>
+        <h1> React Starter Kit! </h1>
+        <h3> Name: {this.state.name} </h3>
+        <ShowList names={this.state.friends} />
+      </div>
+    )
+  }
+});
+
+module.exports = FriendsContainer;
